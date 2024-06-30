f16dada4b03adc78d2ab7cf929e325ee9ff9d5cc
Tal Barda <talbarda8@gmail.com>
2024-06-30 09:33:51 +0300

    Main menu panel

--- Assets/Scripts/Main Menu/MainMenuManager.cs
+++ Assets/Scripts/Main Menu/MainMenuManager.cs
@@ -10,10 +10,13 @@ public class MainMenuManager : MonoBehaviour
 {
     [SerializeField] private TMP_Text _tapToPlayText;
     [SerializeField] private GameObject menuPanel;
+    [SerializeField] private GameObject touchCanvas;
 
     void Start()
     {
         menuPanel.SetActive(true);
+        touchCanvas.SetActive(false); 
+
         _tapToPlayText.transform.DOScale(1.1f, 0.5f).SetLoops(10000, LoopType.Yoyo).SetEase(Ease.InOutFlash);
 
         EventTrigger trigger = menuPanel.GetComponent<EventTrigger>();
@@ -31,5 +34,6 @@ public class MainMenuManager : MonoBehaviour
     public void OnPlayButtonPressed()
     {
         menuPanel.SetActive(false);
+        touchCanvas.SetActive(true);
     }
 }
