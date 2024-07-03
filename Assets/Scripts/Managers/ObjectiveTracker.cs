baaaa5ec81f10b189547018c43ed8ac775c34a26
Tal Barda <talbarda8@gmail.com>
2024-07-03 11:37:29 +0300

    Star-based progression system implementation

---
+++ Assets/Scripts/Managers/ObjectiveTracker.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectiveTracker : MonoBehaviour
+{
+    public LevelManager levelManager;
+
+    private void Update()
+    {
+    //     if (/* condition for Objective1 */)
+    //     {
+    //         levelManager.OnObjectiveTrigger1();
+    //     }
+    //     if (/* condition for Objective2 */)
+    //     {
+    //         levelManager.OnObjectiveTrigger2();
+    //     }
+    //     if (/* condition for Objective3 */)
+    //     {
+    //         levelManager.OnObjectiveTrigger3();
+    //     }
+    // }
+}
