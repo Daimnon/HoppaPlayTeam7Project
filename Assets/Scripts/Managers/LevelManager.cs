b3fa704d4e2fdbedc6cce3e444385965400fc3e2
Daimnon <49341329+Daimnon@users.noreply.github.com>
2024-07-05 03:34:09 +0300

    Nice build ready

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -26,6 +26,8 @@ public class LevelManager : MonoBehaviour
     [SerializeField] private int _currentProgression = 0;
     [SerializeField] private float _timeLimit = 0.0f;
 
+    private bool _hasLost = false;
+
     private void OnEnable()
     {
         EventManager.OnProgressMade += OnProgressMade;
@@ -72,6 +74,9 @@ public class LevelManager : MonoBehaviour
 
     private void HandleLoseCondition()
     {
+        if (_hasLost)
+            return;
+
         _timeLimit -= Time.deltaTime;
         EventManager.InvokeTimerChange(_timeLimit);
 
@@ -82,6 +87,13 @@ public class LevelManager : MonoBehaviour
     {
         // do lose condition logic
         _loseCanvas.SetActive(true);
+        EventManager.InvokeLose();
+        _hasLost = true;
+    }
+
+    public void ReloadLevel()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
     }
 
     public int GetCondition(string objectiveName)
